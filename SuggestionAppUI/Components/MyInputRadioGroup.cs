1728841659c4582d95d924312d1983d7204059d9
yapzhanrui2 <yapzhanrui22@gmail.com>
2022-12-12 21:46:26 +1100

    Added suggestion details html and create suggestion html.

---
+++ SuggestionAppUI/Components/MyInputRadioGroup.cs
@@ -0,0 +1,19 @@
+﻿namespace SuggestionAppUI.Components;
+using Microsoft.AspNetCore.Components.Forms;
+public class MyInputRadioGroup<TValue> : InputRadioGroup<TValue>
+{
+    private string _name;
+    private string _fieldClass;
+
+    protected override void OnParametersSet()
+    {
+        var fieldClass = EditContext?.FieldCssClass(FieldIdentifier) ?? string.Empty;
+        if(fieldClass != _fieldClass || Name != _name)
+        {
+            _fieldClass = fieldClass;
+            _name = Name;
+            base.OnParametersSet();
+        }
+        
+    }
+}
